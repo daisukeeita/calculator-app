962618f9b5e306562ebf14cfde02d2a45a3fa091
JAVV <vinas.johnandrei@gmail.com>
2024-09-21 20:10:40 +0800

    updating the parent component for the child to use the contexts

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { PressedKeyContextProvider } from './contexts/PressedKeyContext.tsx'
+import { InputValueContextProvider } from './contexts/inputValueContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<PressedKeyContextProvider>
-			<App />
-		</PressedKeyContextProvider>
+		<InputValueContextProvider>
+			<PressedKeyContextProvider>
+				<App />
+			</PressedKeyContextProvider>
+		</InputValueContextProvider>
 	</StrictMode>
 )
