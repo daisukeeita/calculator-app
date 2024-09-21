6cb5743b8791a4a28d02504f4ae26a42754b4745
JAVV <vinas.johnandrei@gmail.com>
2024-09-21 20:09:27 +0800

    updating the main component to render the button components dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import React from 'react'
 import ButtonComponent from './components/buttonComponent.tsx'
+import InputComponent from './components/inputComponent.tsx'
+
+const buttonName = [
+	['/', '*', '-'],
+	['1', '2', '3'],
+	['4', '5', '6'],
+	['7', '8', '9'],
+	['0', '.', '='],
+	['+', 'C']
+]
 
 const App = (): React.JSX.Element => {
 	return (
 		<div>
-			<ButtonComponent name={'1'} value={1} />
+			<div>
+				<InputComponent />
+			</div>
+			<div>
+				{buttonName.flat().map(
+					([el, i]: string): React.JSX.Element => (
+						<ButtonComponent key={i} name={el} value={el} />
+					)
+				)}
+			</div>
 		</div>
 	)
 }
