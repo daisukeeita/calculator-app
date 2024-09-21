2b1bc5d3ca2e4826987bf9eacafd31dd0057d6af
JAVV <vinas.johnandrei@gmail.com>
2024-09-21 20:06:52 +0800

    updating the input component to display the buttons pressed

--- src/components/inputComponent.tsx
+++ src/components/inputComponent.tsx
@@ -1,5 +1,14 @@
-const inputComponent = () => {
-	return <div>inputComponent</div>
+import { useContext } from 'react'
+import { InputValueContext } from '../contexts/inputValueContext.tsx'
+
+const InputComponent = (): React.JSX.Element => {
+	const { inputValue } = useContext(InputValueContext)
+
+	return (
+		<div className='border border-blue-300 rounded-lg outline-none h-20 p-2 flex flex-row-reverse items-end tracking-wider'>
+			<span className='text-2xl text-slate-300'>{inputValue}</span>
+		</div>
+	)
 }
 
-export default inputComponent
+export default InputComponent
